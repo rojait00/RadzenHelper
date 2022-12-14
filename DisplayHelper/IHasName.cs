bddc74b21c1cbd13b06040b04125780f25a5a71e
Robin Jahke <robin.jahke@adesso.de>
2022-12-14 01:15:03 +0100

    Added and use IHasName

---
+++ DisplayHelper/IHasName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadzenHelper.DisplayHelper
+{
+    public interface IHasName
+    {
+        public string Name { get; set; }
+    }
+}
